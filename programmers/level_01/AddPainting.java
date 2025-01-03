7bafe6ab873d936035606e5dfb70536a4313ea67
sora0319 <sojeoung536985@gmail.com>
2025-01-03 23:12:34 +0900

    feat : 덧칠하기 문제

---
+++ programmers/level_01/AddPainting.java
@@ -0,0 +1,26 @@
+public class AddPainting {
+    public static void main(String[] args) {
+        Solution S = new Solution();
+        int answer = S.solution(8,4,new int[]{2,3,6});
+        System.out.println(answer);
+    }
+    private static class Solution {
+        public int solution(int n, int m, int[] section) {
+            int answer = 0;
+            int color = 0;
+            int i = 0;
+
+            while(i < section.length){
+                if(color >= section[i]){
+                    i++;
+                    continue;
+                }
+                color = section[i] + m - 1;
+                answer++;
+                i++;
+            }
+
+            return answer;
+        }
+    }
+}
