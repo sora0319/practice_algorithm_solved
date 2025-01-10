d2d50cecb9275edb89e044b3cda4639dba491e19
sora0319 <sojeoung536985@gmail.com>
2025-01-10 23:57:44 +0900

    feat : 신고 결과 받기 미완

--- programmers/KAKAO/2022_RECRUITMENT/ReportList.java
+++ programmers/KAKAO/2022_RECRUITMENT/ReportList.java
@@ -1,2 +1,56 @@
+import java.util.*;
+
 public class ReportList {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+        public int[] solution(String[] id_list, String[] report, int k) {
+            HashMap<String, HashSet<String>> request = new HashMap<>(id_list.length);
+            HashMap<String, Integer> warning = new HashMap<>(id_list.length);
+            List<String> people = new ArrayList<>();
+            List<Integer> result = new ArrayList<>();
+
+            for(int i = 0; i < id_list.length; i++){
+                request.put(id_list[i], new HashSet<>());
+                warning.put(id_list[i], 0);
+            }
+
+            for(int i = 0; i < report.length; i++){
+                String[] r = report[i].split(" ");
+                HashSet temp = request.get(r[0]);
+                temp.add(r[1]);
+
+                request.put(r[0], temp);
+                warning.put(r[1], warning.get(r[0]) + 1);
+            }
+
+            for(int i = 0; i < id_list.length; i++){
+                if(warning.get(id_list[i]) >= k){
+                    people.add(id_list[i]);
+                }
+            }
+
+            for(int i = 0; i < id_list.length; i++){
+                if(!request.containsKey(id_list[i])) {
+                    result.add(0);
+                    continue;
+                }
+                Set temp = request.get(id_list[i]);
+                int cnt = 0;
+                for(String p : people){
+                    if(temp.contains(p)) cnt++;
+                }
+                result.add(0);
+            }
+
+            for()
+
+                int[] answer = new int[result.size()];
+            for(int i = 0; i < answer.length; i++){
+                answer[i] = result.get(i);
+            }
+            return answer;
+        }
+    }
 }
