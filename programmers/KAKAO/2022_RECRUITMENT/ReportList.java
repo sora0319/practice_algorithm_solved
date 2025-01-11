2543beb2a9244ce1ab94a5b78f96794cd9207461
sora0319 <sojeoung536985@gmail.com>
2025-01-11 23:54:49 +0900

    feat : 신고 결과 받기 문제

--- programmers/KAKAO/2022_RECRUITMENT/ReportList.java
+++ programmers/KAKAO/2022_RECRUITMENT/ReportList.java
@@ -2,9 +2,11 @@ import java.util.*;
 
 public class ReportList {
     public static void main(String[] args) {
-
+        Solution s = new Solution();
+        int[] answer = s.solution(new String[]{"muzi", "frodo", "apeach", "neo"}, new String[]{"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"}, 2);
+        System.out.println(Arrays.toString(answer));
     }
-    class Solution {
+    private static class Solution {
         public int[] solution(String[] id_list, String[] report, int k) {
             HashMap<String, HashSet<String>> request = new HashMap<>(id_list.length);
             HashMap<String, Integer> warning = new HashMap<>(id_list.length);
@@ -19,10 +21,13 @@ public class ReportList {
             for(int i = 0; i < report.length; i++){
                 String[] r = report[i].split(" ");
                 HashSet temp = request.get(r[0]);
-                temp.add(r[1]);
+                if(!temp.contains(r[1])){
+                    temp.add(r[1]);
 
-                request.put(r[0], temp);
-                warning.put(r[1], warning.get(r[0]) + 1);
+                    request.put(r[0], temp);
+                    warning.put(r[1], warning.get(r[1]) + 1);
+                }
+
             }
 
             for(int i = 0; i < id_list.length; i++){
@@ -41,12 +46,12 @@ public class ReportList {
                 for(String p : people){
                     if(temp.contains(p)) cnt++;
                 }
-                result.add(0);
+                result.add(cnt);
             }
 
-            for()
 
-                int[] answer = new int[result.size()];
+
+            int[] answer = new int[result.size()];
             for(int i = 0; i < answer.length; i++){
                 answer[i] = result.get(i);
             }
